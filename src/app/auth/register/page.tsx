e9fe73e00afac10a7380f60f4bc9e369faf17aa9
Aphiwe Mntungwa <aphiwemntungwa66@gmail.com>
2024-07-07 22:31:36 +0200

    register stepper

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
-import { TextField, Button, Typography } from '@mui/material';
+import { TextField, Autocomplete, Button, Typography, Stepper, StepLabel, Step, Box } from '@mui/material';
+import Input from '@mui/joy/Input';
 
 type Inputs = {
     studentNumber: string;
@@ -31,9 +32,32 @@ const validationSchema = Yup.object().shape({
         .min(6, 'Password must be at least 6 characters'),
 });
 
+const steps = ['Add Residence Information', 'Create Account'];
+
 const RegisterStudent = () => {
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [resName, setResName] = React.useState<any | null>(residences[0]);
+    const [roomNumber, setRoomNumber] = React.useState<number | null>(1);
+    const inputRef = React.useRef<HTMLInputElement | null>(null);
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
     const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
         resolver: yupResolver(validationSchema)
+    });
+
+    const options = residences.map((option) => {
+        const firstLetter = option.title[0].toUpperCase();
+        return {
+            firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
+            ...option,
+        };
     });
 
     const onSubmit: SubmitHandler<Inputs> = data => {
@@ -41,69 +65,253 @@ const RegisterStudent = () => {
     }
 
     return (
-        <section className="flex min-h-screen flex-col items-center justify-between pt-20 px-16">
-            <form style={{ minWidth: "50%" }} onSubmit={handleSubmit(onSubmit)}>
-                <Typography sx={{ textDecoration: "underline" }} variant="h4" component="h4">
-                    Register
-                </Typography>
-                <div>
-                    <TextField
-                        fullWidth
-                        label="Student Number"
-                        margin="normal"
-                        {...register("studentNumber")}
-                        error={!!errors.studentNumber}
-                        helperText={errors.studentNumber?.message}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        fullWidth
-                        label="First Name"
-                        margin="normal"
-                        {...register("firstName")}
-                        error={!!errors.firstName}
-                        helperText={errors.firstName?.message}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        fullWidth
-                        label="Last Name"
-                        margin="normal"
-                        {...register("lastName")}
-                        error={!!errors.lastName}
-                        helperText={errors.lastName?.message}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        fullWidth
-                        label="School Email"
-                        type="email"
-                        margin="normal"
-                        {...register("email")}
-                        error={!!errors.email}
-                        helperText={errors.email?.message}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        fullWidth
-                        label="Password"
-                        type="password"
-                        margin="normal"
-                        {...register("password")}
-                        error={!!errors.password}
-                        helperText={errors.password?.message}
-                    />
-                </div>
-                <Button type="submit" variant="outlined" color="primary">
-                    Register
+        <section className="flex min-h-screen flex-col items-center justify-start pt-5 px-16">
+            <Typography variant="h6" component="h6">
+                Create Reshub Account
+            </Typography>
+            <Stepper activeStep={activeStep}>
+                {steps.map((label, index) => {
+                    const stepProps: { completed?: boolean } = {};
+                    const labelProps: {
+                        optional?: React.ReactNode;
+                    } = {};
+
+                    return (
+                        <Step key={label} {...stepProps}>
+                            <StepLabel {...labelProps}>{label}</StepLabel>
+                        </Step>
+                    );
+                })}
+            </Stepper>
+            {activeStep === 0 ? (<Box className="pt-5">
+                <Autocomplete
+                    id="grouped-demo"
+                    options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                    groupBy={(option) => option.firstLetter}
+                    getOptionLabel={(option) => option.title}
+                    sx={{ width: 300 }}
+                    value={resName}
+                    onChange={(event, newValue) => {
+                      setResName(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} label="Residences With categories" />}
+                />
+                <Input
+                    className='mt-5'
+                    sx={{ height: 20 }}
+                    type="number"
+                    value={roomNumber?.toString()}
+                    onChange={(event) => setRoomNumber(parseInt(event.target.value))}
+                    slotProps={{
+                        input: {
+                            ref: inputRef,
+                            min: 1,
+                            max: 4000,
+                            step: 1,
+                        },
+                    }}
+                />
+            </Box>) :
+                (<form style={{ minWidth: "50%" }} onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <TextField
+                            fullWidth
+                            label="Student Number"
+                            margin="normal"
+                            {...register("studentNumber")}
+                            error={!!errors.studentNumber}
+                            helperText={errors.studentNumber?.message}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            label="First Name"
+                            margin="normal"
+                            {...register("firstName")}
+                            error={!!errors.firstName}
+                            helperText={errors.firstName?.message}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            label="Last Name"
+                            margin="normal"
+                            {...register("lastName")}
+                            error={!!errors.lastName}
+                            helperText={errors.lastName?.message}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            label="School Email"
+                            type="email"
+                            margin="normal"
+                            {...register("email")}
+                            error={!!errors.email}
+                            helperText={errors.email?.message}
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            label="Password"
+                            type="password"
+                            margin="normal"
+                            {...register("password")}
+                            error={!!errors.password}
+                            helperText={errors.password?.message}
+                        />
+                    </div>
+                    <Button type="submit" variant="outlined" color="primary">
+                        Register
+                    </Button>
+                </form>)}
+            <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                <Button
+                    color="inherit"
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    sx={{ mr: 1 }}
+                >
+                    Back
                 </Button>
-            </form>
-        </section>
+
+                <Button onClick={handleNext} disabled={activeStep === steps.length - 1}>
+                    Next
+                </Button>
+            </Box>
+        </section >
     );
 };
+
+const residences = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    {
+        title: 'The Lord of the Rings: The Return of the King',
+        year: 2003,
+    },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+    {
+        title: 'The Lord of the Rings: The Fellowship of the Ring',
+        year: 2001,
+    },
+    {
+        title: 'Star Wars: Episode V - The Empire Strikes Back',
+        year: 1980,
+    },
+    { title: 'Forrest Gump', year: 1994 },
+    { title: 'Inception', year: 2010 },
+    {
+        title: 'The Lord of the Rings: The Two Towers',
+        year: 2002,
+    },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { title: 'Goodfellas', year: 1990 },
+    { title: 'The Matrix', year: 1999 },
+    { title: 'Seven Samurai', year: 1954 },
+    {
+        title: 'Star Wars: Episode IV - A New Hope',
+        year: 1977,
+    },
+    { title: 'City of God', year: 2002 },
+    { title: 'Se7en', year: 1995 },
+    { title: 'The Silence of the Lambs', year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: 'Life Is Beautiful', year: 1997 },
+    { title: 'The Usual Suspects', year: 1995 },
+    { title: 'Léon: The Professional', year: 1994 },
+    { title: 'Spirited Away', year: 2001 },
+    { title: 'Saving Private Ryan', year: 1998 },
+    { title: 'Once Upon a Time in the West', year: 1968 },
+    { title: 'American History X', year: 1998 },
+    { title: 'Interstellar', year: 2014 },
+    { title: 'Casablanca', year: 1942 },
+    { title: 'City Lights', year: 1931 },
+    { title: 'Psycho', year: 1960 },
+    { title: 'The Green Mile', year: 1999 },
+    { title: 'The Intouchables', year: 2011 },
+    { title: 'Modern Times', year: 1936 },
+    { title: 'Raiders of the Lost Ark', year: 1981 },
+    { title: 'Rear Window', year: 1954 },
+    { title: 'The Pianist', year: 2002 },
+    { title: 'The Departed', year: 2006 },
+    { title: 'Terminator 2: Judgment Day', year: 1991 },
+    { title: 'Back to the Future', year: 1985 },
+    { title: 'Whiplash', year: 2014 },
+    { title: 'Gladiator', year: 2000 },
+    { title: 'Memento', year: 2000 },
+    { title: 'The Prestige', year: 2006 },
+    { title: 'The Lion King', year: 1994 },
+    { title: 'Apocalypse Now', year: 1979 },
+    { title: 'Alien', year: 1979 },
+    { title: 'Sunset Boulevard', year: 1950 },
+    {
+        title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
+        year: 1964,
+    },
+    { title: 'The Great Dictator', year: 1940 },
+    { title: 'Cinema Paradiso', year: 1988 },
+    { title: 'The Lives of Others', year: 2006 },
+    { title: 'Grave of the Fireflies', year: 1988 },
+    { title: 'Paths of Glory', year: 1957 },
+    { title: 'Django Unchained', year: 2012 },
+    { title: 'The Shining', year: 1980 },
+    { title: 'WALL·E', year: 2008 },
+    { title: 'American Beauty', year: 1999 },
+    { title: 'The Dark Knight Rises', year: 2012 },
+    { title: 'Princess Mononoke', year: 1997 },
+    { title: 'Aliens', year: 1986 },
+    { title: 'Oldboy', year: 2003 },
+    { title: 'Once Upon a Time in America', year: 1984 },
+    { title: 'Witness for the Prosecution', year: 1957 },
+    { title: 'Das Boot', year: 1981 },
+    { title: 'Citizen Kane', year: 1941 },
+    { title: 'North by Northwest', year: 1959 },
+    { title: 'Vertigo', year: 1958 },
+    {
+        title: 'Star Wars: Episode VI - Return of the Jedi',
+        year: 1983,
+    },
+    { title: 'Reservoir Dogs', year: 1992 },
+    { title: 'Braveheart', year: 1995 },
+    { title: 'M', year: 1931 },
+    { title: 'Requiem for a Dream', year: 2000 },
+    { title: 'Amélie', year: 2001 },
+    { title: 'A Clockwork Orange', year: 1971 },
+    { title: 'Like Stars on Earth', year: 2007 },
+    { title: 'Taxi Driver', year: 1976 },
+    { title: 'Lawrence of Arabia', year: 1962 },
+    { title: 'Double Indemnity', year: 1944 },
+    {
+        title: 'Eternal Sunshine of the Spotless Mind',
+        year: 2004,
+    },
+    { title: 'Amadeus', year: 1984 },
+    { title: 'To Kill a Mockingbird', year: 1962 },
+    { title: 'Toy Story 3', year: 2010 },
+    { title: 'Logan', year: 2017 },
+    { title: 'Full Metal Jacket', year: 1987 },
+    { title: 'Dangal', year: 2016 },
+    { title: 'The Sting', year: 1973 },
+    { title: '2001: A Space Odyssey', year: 1968 },
+    { title: "Singin' in the Rain", year: 1952 },
+    { title: 'Toy Story', year: 1995 },
+    { title: 'Bicycle Thieves', year: 1948 },
+    { title: 'The Kid', year: 1921 },
+    { title: 'Inglourious Basterds', year: 2009 },
+    { title: 'Snatch', year: 2000 },
+    { title: '3 Idiots', year: 2009 },
+    { title: 'Monty Python and the Holy Grail', year: 1975 },
+];
 
 export default RegisterStudent;
